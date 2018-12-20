ee2f513e8329fa3802acb63db684c9033960263e
OMEGAd0r4 <45790881+OMEGAd0r4@users.noreply.github.com>
2018-12-20 16:27:57 +0700

    Create mute.js

---
+++ commands/voltpvp/mute.js
@@ -0,0 +1,64 @@
+const Discord = require("discord.js");
+const commando = require('discord.js-commando')
+const prefix = ">";
+const bot = new commando.Client({
+  commandPrefix: prefix
+});
+
+class muteCommand extends commando.Command {
+    constructor(client) 
+    {
+      super(client, {
+        name: 'mute', 
+        group: 'voltpvp',
+        memberName: 'mute',
+        description: "Temporarily mutes a user"
+      });
+    }
+
+    async run(message, args)
+    {
+        var tempmuteargs = message.content.slice(prefix.length).split(/ + /); //MAIN ARGS
+        var tempmuteuser = message.guild.member(message.mentions.users.first() || message.guild.members.get(tempmuteargs[0]));
+        var tempmutereason = tempmuteargs.join(" ").slice(26);
+        var tempmuterole = message.guild.roles.find(`name`, "Muted");
+        var supportteamerole = message.guild.roles.find(`name`, "SUPPORT TEAM");
+
+        if (!tempmuterole) return message.channel.send("Where cannot find a role called 'Muted'.")
+
+        if (!tempmuteuser) return message.channel.send({embed: new Discord.RichEmbed()
+            .setDescription(":x: **Missing args**")
+            .setColor("#FF4040")
+            .addField("->", "/mute [User] [Reason]")});
+
+        if (!tempmutereason) return message.channel.send({embed: new Discord.RichEmbed()
+            .setDescription(":x: **Missing args**")
+            .setColor("#FF4040")
+            .addField("->", "/mute [User] [Reason]")});
+
+        if (!message.member.roles.has(supportteamerole.id)) return message.channel.send("Insufficient permission. You do not have permission to mute others users")
+
+        if (tempmuteuser.roles.has(supportteamerole.id)) return message.channel.send("Insufficient permission. You do not have permission to mute the support team members!")
+
+            var tempmuteembed = new Discord.RichEmbed()
+                .setTitle("**VoltPvP | Mute**")
+                .setColor("#FFDF00")
+                .setDescription(`${tempmuteuser}, Muted by ${message.author}`)
+                .addField(`**REASON:**`, "->" + tempmutereason)
+                .setTimestamp()
+
+                let logschannel = message.guild.channels.find(`name`, "logs");
+                if(!logschannel) return message.channel.send("Couldn't find the logs channel");
+
+                logschannel.send(tempmuteembed).then(tempmuteuser.addRole(tempmuterole.id));
+
+                message.delete();
+
+                message.channel.send(`${tempmuteuser} has been sucessfully been muted.`)
+
+                tempmuteuser.sendMessage(tempmuteuser + " Hey, you have been muted from the VoltPvP Network for " + tempmutereason)
+        
+    }
+}
+
+module.exports = muteCommand;
