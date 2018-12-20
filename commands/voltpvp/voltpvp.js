74e44755b2e28bc2741907fb1d752b5a2f125a44
OMEGAd0r4 <45790881+OMEGAd0r4@users.noreply.github.com>
2018-12-20 16:31:53 +0700

    Create voltpvp.js

---
+++ commands/voltpvp/voltpvp.js
@@ -0,0 +1,32 @@
+const Discord = require("discord.js");
+const commando = require('discord.js-commando')
+const prefix = ">";
+const bot = new commando.Client({
+  commandPrefix: prefix
+});
+
+class infoCommand extends commando.Command {
+    constructor(client) 
+    {
+      super(client, {
+        name: 'info', 
+        group: 'voltpvp',
+        memberName: 'info',
+        description: "Shows voltpvp's server information"
+      });
+    }
+
+    async run(message)
+    {
+        message.channel.send({embed: new Discord.RichEmbed()
+            .setTitle("**VoltPvP [] Information**")
+            .setColor("#FFDF00")
+            .addField("**__Twitter__**", "-> soon")
+            .addField("**__Discord__**", " -> https://discord.gg/rzKJNyg")
+            .addField("**__Store__**", "-> https://voltpvpasia.buycraft.net")
+            .setImage("https://cdn.discordapp.com/attachments/523073883427831809/525212118882975755/Volt_old_logo.png")
+            .setTimestamp()})
+    }
+}
+
+module.exports = infoCommand
