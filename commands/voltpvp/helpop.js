e1d55651fd4f9c70e79af343bfbef557f8a4db5c
OMEGAd0r4 <45790881+OMEGAd0r4@users.noreply.github.com>
2018-12-20 16:30:32 +0700

    Update helpop.js

--- commands/voltpvp/helpop.js
+++ commands/voltpvp/helpop.js
@@ -5,41 +5,47 @@ const bot = new commando.Client({
   commandPrefix: prefix
 });
 
-class helpopCommand extends commando.Command {
-  constructor(client) 
-  {
-    super(client, {
-      name: 'helpop', 
-      group: 'voltpvp',
-      memberName: 'helpop',
-      description: "Provides a user's request"
-    });
-  }
+class unmuteCommand extends commando.Command {
+    constructor(client) 
+    {
+      super(client, {
+        name: 'unmute', 
+        group: 'voltpvp',
+        memberName: 'unmute',
+        description: "Unmutes a muted user"
+      });
+    }
 
-  async run(message,args)
-  {
-    let requestargs = message.content.slice(prefix.length).split(/ + /); //MAIN ARGS
-        let requestmessage = requestargs.join(" ").slice(7);
-        if (!requestmessage) return message.channel.send({embed: new Discord.RichEmbed()
-          .setDescription(":x: **Missing args**")
-          .setColor("#FF4040")
-          .addField("->", ">helpop [request]")});
+    async run(message, args)
+    {
+        var unmuteargs = message.content.slice(prefix.length).split(/ + /); //MAIN ARGS
+        var unmuteuser = message.guild.member(message.mentions.users.first() || message.guild.members.get(unmuteargs[0]));
+        var unmuterole = message.guild.roles.find(`name`, "Muted");
+        var supportteamerole = message.guild.roles.find(`name`, "SUPPORT TEAM");
 
-            var requestembed = new Discord.RichEmbed()
-            .setTitle('**VoltPvP | Requestop**')
-            .setColor("#FFDF00")
-            .addField('**__Request By__**', `${message.author} with the ID: ${message.author.id}`)
-            .addField('**__User Request__**', requestmessage)
-            .setTimestamp()
+        if (!unmuterole) return message.channel.send("Please make a 'Muted' role to proceed")
+
+        if (!unmuteuser) return message.channel.send({embed: new Discord.RichEmbed()
+            .setDescription(":x: **Missing args**")
+            .setColor("#FF4040")
+            .addField("->", ">unmute [User]")});
+
+        if(!message.member.roles.has(supportteamerole.id)) return message.channel.send("Insufficient permission. You do not have permission to unmute other users")
+
+        if(unmuteuser.roles.has(supportteamerole.id)) return message.channel.send("Insufficient permission. You do not have permission to unmute the support team members!");
+
+        var unmuteembed = new Discord.RichEmbed()
+            .setColor("#4286f4")
+            .setTitle('**VoltPvP | Unmute**')
+            .setDescription(`${unmuteuser} has been unmuted by ${message.author}`, `->`)
+            
 
             let logschannel = message.guild.channels.find(`name`, "logs");
             if(!logschannel) return message.channel.send("Couldn't find the logs channel");
 
-            message.delete(3000);
-            logschannel.send(requestembed);
-
-        message.reply('Thanks for your request. We will contact you within 24h')
-  }
+            message.channel.send(`${unmuteuser} has been sucessfully been unmuted`)
+            logschannel.send(unmuteembed).then(unmuteuser.removeRole(unmuterole.id));
+    }
 }
 
-module.exports = helpopCommand
+module.exports = unmuteCommand;
