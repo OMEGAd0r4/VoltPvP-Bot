46b100116913fdbe7b9d9edeb925dcd60fa1b13c
OMEGAd0r4 <45790881+OMEGAd0r4@users.noreply.github.com>
2018-12-21 09:45:56 +0700

    Update voltpvp.js

--- commands/voltpvp/voltpvp.js
+++ commands/voltpvp/voltpvp.js
@@ -5,28 +5,50 @@ const bot = new commando.Client({
   commandPrefix: prefix
 });
 
-class infoCommand extends commando.Command {
+class ticketCommand extends commando.Command {
     constructor(client) 
     {
       super(client, {
-        name: 'info', 
+        name: 'new', 
         group: 'voltpvp',
-        memberName: 'info',
-        description: "Shows voltpvp's server information"
+        memberName: 'new',
+        description: "Creates a support ticket"
       });
     }
-
-    async run(message)
+    async run(message, args)
     {
-        message.channel.send({embed: new Discord.RichEmbed()
-            .setTitle("**VoltPvP [] Information**")
+        if (!message.channel.name.startsWith(`helpdesk`)) return message.channel.send(`You can't use this command outside of a ticket channel.`);
+        const reason = message.content.split(" ").slice(4).join(" ");
+        if (!reason) return message.channel.send({embed: new Discord.RichEmbed()
+            .setDescription(":x: **Missing args**")
+            .setColor("#FF4040")
+            .addField("->", ">new [subject]")});
+        if (message.guild.channels.exists("name", "ticket-" + message.author.id)) return message.channel.send("You already have a ticket open.");
+        message.guild.createChannel(`ticket-${message.author.id}`, "text").then(c => {
+            let role = message.guild.roles.find("name", "SUPPORT TEAM");
+            let role2 = message.guild.roles.find("name", "@everyone");
+            c.overwritePermissions(role, {
+                SEND_MESSAGES: true,
+                READ_MESSAGES: true
+            });
+            c.overwritePermissions(role2, {
+                SEND_MESSAGES: false,
+                READ_MESSAGES: false
+            });
+            c.overwritePermissions(message.author, {
+                 SEND_MESSAGES: true,
+                 READ_MESSAGES: true
+            });
+            message.channel.send(`:white_check_mark: Your ticket has been created, ${c.name}.`);
+            const embed = new Discord.RichEmbed()
             .setColor("#FFDF00")
-            .addField("**__Twitter__**", "-> soon")
-            .addField("**__Discord__**", " -> https://discord.gg/rzKJNyg")
-            .addField("**__Store__**", "-> https://voltpvpasia.buycraft.net")
-            .setImage("https://cdn.discordapp.com/attachments/523073883427831809/525212118882975755/Volt_old_logo.png")
-            .setTimestamp()})
-    }
+            .addField(`Hey ${message.author.username}!, Your support ticket has been created, please explain th reason for this ticket and we will respond within 24h.`, `Thanks for your patience.`)
+            .addField("**__Subject__**", reason)
+            .setTimestamp();
+            c.send({ embed: embed });
+        }).catch(console.error);
+        }
 }
 
-module.exports = infoCommand
+module.exports = ticketCommand;
+
