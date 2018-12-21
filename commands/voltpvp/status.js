cedfa3f2c9e98b999df0739d22c3ae70d06a3941
OMEGAd0r4 <45790881+OMEGAd0r4@users.noreply.github.com>
2018-12-21 11:18:32 +0700

    Create status.js

---
+++ commands/voltpvp/status.js
@@ -0,0 +1,58 @@
+const commando = require('discord.js-commando')
+const Discord = require('discord.js');
+const prefix = ">";
+const bot = new commando.Client({
+    commandPrefix: prefix
+});
+
+class statusCommand extends commando.Command {
+    constructor(client)
+    {
+      super(client, {
+        name: 'status',
+        group: 'network',
+        memberName: 'status',
+        description: 'Shows the status of the Zilex Network'
+      });
+    }
+
+    async run(message)
+    {
+        //npm install request --save
+        var request = require("request");
+        var mcIP = "voltpvp.mymc.io";
+        var mcPort = "";
+
+        var url = "http://mcapi.us/server/status?ip=" + mcIP + "&port" + mcPort;
+        request(url, function(err, response, body) {
+            if (err) message.channel.send(err);
+
+            body = JSON.parse(body);
+            
+            var status = `The VoltPvP Network is currently offline`;
+            if(body.players.now){
+                message.channel.send({embed: new Discord.RichEmbed()
+                    .setTitle("**VoltPvP | Status**")
+                    .setColor("#FFDF00")
+                    .addField("The VoltPvP network is currently online with,", `${body.players.now} people playing`)
+                    .setThumbnail("https://cdn.discordapp.com/attachments/523073883427831809/525212118882975755/Volt_old_logo.png")})
+            }
+            if(!body.players.now){
+                message.channel.send({embed: new Discord.RichEmbed()
+                    .setTitle("**VoltPvP | Status**")
+                    .setColor("#4286f4")
+                    .addField("The VoltPvP network is currently online with,", "-> 0 players")
+                    .setThumbnail("https://cdn.discordapp.com/attachments/523073883427831809/525212118882975755/Volt_old_logo.png")})
+            }
+            else{
+                message.channel.send({embed: new Discord.RichEmbed()
+                    .setTitle("**VoltPvP | Status**")
+                    .setColor("#FFDF00")
+                    .addField("The VoltPvP network is currently under maintenance", "->")
+                    .setThumbnail("https://cdn.discordapp.com/attachments/523073883427831809/525212118882975755/Volt_old_logo.png")})
+            }
+        })
+    }
+}
+
+module.exports = statusCommand;
