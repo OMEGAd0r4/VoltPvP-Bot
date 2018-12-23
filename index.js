ace9847687f6b168d3ded80e9a5d800fbea4c790
OMEGAd0r4 <45790881+OMEGAd0r4@users.noreply.github.com>
2018-12-23 13:44:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -23,3 +23,41 @@ bot.registry.registerGroup('voltpvp', 'voltpvp')
 bot.registry.registerCommandsIn(__dirname + "/commands");
 bot.registry.registerDefaults();
 //REGISTIES
+
+//RANKS INFORMATION
+bot.on('message', (message) => {
+    if (message.content == "$Vertus")
+    message.channel.send({embed: new Discord.RichEmbed()
+        .setTitle("**Vertus | Rank**")
+        .setDescription("Buying this rank will allow you to have the permissions below")
+        .setColor("#FFDF00")
+        .addField("**__Nick", "/nick__**")
+        .addField("**__Fly", "/fly__**")
+        .addField("**__Host public parties__**", "->")})
+})
+
+bot.on('message', (message) =>{
+    if (message.content == "$NovA")
+    message.channel.send({embed: new Discord.RichEmbed()
+        .setTitle("**Nova | Rank**")
+        .setColor("#FFDF00")
+        .setDescription("Buying this rank will allow you to have the permissions below")
+        .addField("**__Nick__**", "/nick")
+        .addField("**__Fly__**", "/fly")
+        .addField("**__Host public parties__**", "->")
+        .addField("**__Host sumo events__**", "->")})
+})
+
+bot.on('message', (message) => {
+    if (message.content == "$Volt")
+    message.channel.send({embed: new Discord.RichEmbed()
+        .setTitle("**Volt | Rank**")
+        .setColor("#FFDF00s")
+        .setDescription("Buying this rank will allow you to have the permissions below")
+        .addField("**__Nick__**", "/nick")
+        .addField("**__Fly__**", "/fly")
+        .addField("**__Host public parties__**", "->")
+        .addField("**__Host sumo events__**", "->")
+        .addField("**__Host KOTH events__**", "->")})
+})
+//RANKS INFORMATION
