b89ebb6c9ea616ca6b08e647ac2513802b489199
OMEGAd0r4 <45790881+OMEGAd0r4@users.noreply.github.com>
2018-12-26 19:42:08 +0700

    Update unmute.js

--- commands/voltpvp/unmute.js
+++ commands/voltpvp/unmute.js
@@ -21,7 +21,6 @@ class unmuteCommand extends commando.Command {
         if (!message.channel.name.startsWith(`commands`))
       {
         message.channel.send(`You can't use this command outside of the commands channel.`);
-        message.delete().catch(O_o=>{});
       }
         var unmuteargs = message.content.slice(prefix.length).split(/ + /); //MAIN ARGS
         var unmuteuser = message.guild.member(message.mentions.users.first() || message.guild.members.get(unmuteargs[0]));
