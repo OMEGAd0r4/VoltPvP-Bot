f112f65aa895ca9710b8eb04e42d6682d6381136
OMEGAd0r4 <45790881+OMEGAd0r4@users.noreply.github.com>
2018-12-26 19:33:58 +0700

    Update mute.js

--- commands/voltpvp/mute.js
+++ commands/voltpvp/mute.js
@@ -20,8 +20,8 @@ class muteCommand extends commando.Command {
     {
         if (!message.channel.name.startsWith(`commands`))
       {
-        message.delete().catch(O_o=>{});
         message.channel.send(`You can't use this command outside of the commands channel.`);
+        message.delete().catch(O_o=>{});
       }
         var tempmuteargs = message.content.slice(prefix.length).split(/ + /); //MAIN ARGS
         var tempmuteuser = message.guild.member(message.mentions.users.first() || message.guild.members.get(tempmuteargs[0]));
