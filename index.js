eef231d64db903c57a2f7a56962ed764c33a7584
OMEGAd0r4 <45790881+OMEGAd0r4@users.noreply.github.com>
2018-12-27 10:13:48 +0700

    Update index.js

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ bot.on('message', (message) => {
         .addField("**__Nick__**", "/nick")
         .addField("**__Fly__**", "/fly")
         .addField("**__Host public parties__**", "[Ingame]")})
+    if (!message.channel.name.startsWith(`commands`)) return message.channel.send("You can't use the info command outside of the commands channel.");
     
 })
 
@@ -47,7 +48,7 @@ bot.on('message', (message) =>{
         .addField("**__Fly__**", "/fly")
         .addField("**__Host public parties__**", "[Ingame]")
         .addField("**__Host sumo events__**", "/hostevent sumo")})
-    
+    if (!message.channel.name.startsWith(`commands`)) return message.channel.send("You can't use the info command outside of the commands channel.");
 })
 
 bot.on('message', (message) => {
@@ -61,6 +62,6 @@ bot.on('message', (message) => {
         .addField("**__Host public parties__**", "[Ingame]")
         .addField("**__Host sumo events__**", "/hostevent sumo")
         .addField("**__Host KOTH events__**", "/hostevent koth")})
-    
+    if (!message.channel.name.startsWith(`commands`)) return message.channel.send("You can't use the info command outside of the commands channel.");
 })
 //RANKS INFORMATION
